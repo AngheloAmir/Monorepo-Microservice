2b74d63eed7427471d4d4049b0822074bbe8f995
angheloamir <angheloamir@gmail.com>
2026-01-01 02:28:35 +0800

    adding repo tool

--- _tool/tooldata/repository.js
+++ _tool/tooldata/repository.js
@@ -1,4 +1,15 @@
-
-const repository = {
-    
-}
+const repository = [
+    {
+        name:        "temp repo",
+        description: "this is a temp repo for testing",
+        type:        "frontend",
+        path:        "../frontend/temp",
+        branch:      "master",
+        devurl:      "http://localhost:3000",
+        produrl:     "http://localhost:3000",
+        startcmd:    "npm run dev",
+        stopcmd:     "npm run stop",
+        buildcmd:    "npm run build",
+        init:        "npm create react-app ."
+    }
+]
