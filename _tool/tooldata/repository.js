f12f39cfcf9df2d21a36baf1a18334c6dec35f5c
angheloamir <angheloamir@gmail.com>
2026-01-01 19:16:16 +0800

    adjusted the template

--- _tool/tooldata/repository.js
+++ _tool/tooldata/repository.js
@@ -3,10 +3,40 @@
  */
 
 const repository = {
-    "backend":  [],
+    "backend": [
+        {
+            "name": "my-service",
+            "description": "",
+            "icon": "fas fa-cube",
+            "type": "backend",
+            "path": "../backend/my-service",
+            "branch": "master",
+            "devurl": "http://localhost:3000",
+            "produrl": "",
+            "startcmd": "npm run dev",
+            "stopcmd": "npm run stop",
+            "buildcmd": "npm run build",
+            "template": ""
+        }
+    ],
     "frontend": [],
     "database": [],
-    "service":  []
+    "service": [
+        {
+            "name": "my-service",
+            "description": "",
+            "icon": "fas fa-cube",
+            "type": "service",
+            "path": "../service/my-service",
+            "branch": "master",
+            "devurl": "http://localhost:3000",
+            "produrl": "",
+            "startcmd": "npm run dev",
+            "stopcmd": "npm run stop",
+            "buildcmd": "npm run build",
+            "template": ""
+        }
+    ]
 }
 
 module.exports = repository;
