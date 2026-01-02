5c578f98dcce177e008f815666ce6d2c2ac8052e
angheloamir <angheloamir@gmail.com>
2026-01-02 20:07:25 +0800

    addding the CRUD

---
+++ _tool/tooldata/crud.js
@@ -0,0 +1,49 @@
+const crud = [
+    {
+        category: "Authentication",
+        items: [
+            {
+                label: "logout",
+                route: "/api/auth/logout",
+                methods: "POST",
+                description: "Invalidates the user's session token.",
+                sampleInput: '{}',
+                suggested: [],
+                expectedOutcome: '{\n  "message": "Logout successful"\n}',
+                availableFor: "public"
+            },
+            {
+                label: "logout",
+                route: "/api/auth/logout",
+                methods: "POST",
+                description: "Invalidates the user's session token.",
+                sampleInput: '{}',
+                suggested: [
+                    { name: "Admin",   content:   '{\n  "email": "admin@admin.com",\n  "password": "Admin123"\n}' },
+                    { name: "CusRaRa", content: '{\n  "email": "CusRaRa@customer.com",\n  "password": "CusRaRa526"\n}' },
+                    { name: "CusTaGe", content: '{\n  "email": "CusTaGe@customer.com",\n  "password": "CusTaGe789"\n}' }
+                ],
+                expectedOutcome: '{\n  "message": "Logout successful"\n}',
+                availableFor: "public"
+            },
+        ]
+    },
+
+     {
+        category: "Another one for test",
+        items: [
+            {
+                label: "logout",
+                route: "/api/auth/logout",
+                methods: "POST",
+                description: "Invalidates the user's session token.",
+                sampleInput: '{}',
+                suggested: [],
+                expectedOutcome: '{\n  "message": "Logout successful"\n}',
+                availableFor: "private"
+            },
+        ]
+    },
+]
+
+module.exports = crud;
