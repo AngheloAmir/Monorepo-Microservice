c13caa4f0bfeb0b6e80ca6be1753e0a3e93a80b0
angheloamir <angheloamir@gmail.com>
2026-01-07 23:46:43 +0800

    automated tester

--- _tool/tooldata/crud.js
+++ _tool/tooldata/crud.js
@@ -11,7 +11,7 @@ const crud = [
                 "description": "Ping the tool server to check if it is running.",
                 "sampleInput": "{}",
                 "suggested": [],
-                "expectedOutcome": "# Please set the root url to localhost:3200 first\n\n# You should see the word \"pong\" as a message \n\n{\n  \"message\": \"pong\"\n}",
+                "expectedOutcome": "# You should see the word \"pong\" as a message \n\n{\n  \"message\": \"pong\"\n}",
                 "availableFor": "public"
             },
             {
@@ -27,7 +27,7 @@ const crud = [
                         "content": "{\n    \"name\": \"Demo Customer\",\n    \"email\": \"CusRaRa@customer.com\",\n    \"phone1\": \"123456789\",\n    \"phone2\": \"987654321\",\n    \"city\": \"randomw1\",\n    \"state\": \"ultra state\",\n    \"zip\": \"12345\",\n    \"country\": \"mega country\",\n    \"icon\": \"test icon\",\n    \"gender\": \"female\",\n    \"delivery_notes\": \"Make sure that it is packed correctly\"\n}"
                     }
                 ],
-                "expectedOutcome": "# Please set the root url to localhost:3200 first\n\n# Note \nYou should see the mirror of your inputs",
+                "expectedOutcome": "# Note \nYou should see the mirror of your inputs",
                 "availableFor": "public"
             },
             {
@@ -48,7 +48,7 @@ const crud = [
                         "content": "{}"
                     }
                 ],
-                "expectedOutcome": "# Please set the root url to localhost:3200 first\n\n# Note \nYou should see the stream of words",
+                "expectedOutcome": "# Note \nYou should see the stream of words",
                 "availableFor": "public"
             }
         ]
